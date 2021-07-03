a5caaf928ec3d08a701148c043c624209a3953ca
Arpit <inboxarp90@gmail.com>
2021-07-03 22:48:34 +0530

    Added location example, currently not working, needs valid api key

--- realestate/urls.py
+++ realestate/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('pages.urls')),
+    path('', include('pages.urls'), name='pages'),
     path('listings/', include('listings.urls')),
     path('accounts/', include('accounts.urls')),
     path('contacts/', include('contacts.urls')),
